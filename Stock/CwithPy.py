55bc609d802806ad111b2f4e73ee09166c4e6987
Don Pinguino <124198546+Pingui222@users.noreply.github.com>
2026-02-20 11:41:06 +0100

    Update CwithPy.py

--- Stock/CwithPy.py
+++ Stock/CwithPy.py
@@ -1,1 +1,17 @@
-#Hola
+import PySerial
+import time 
+#Example values which will be assigned to our arduino's values
+brate=115200
+restT=.1
+puerto="Port4"
+#Connect our arduino or ESP o las dos via a serial port:
+arduino = serial.Serial(port=puerto,baudrate=brate , timeout=restT) 
+def extract_data(x): 
+	   arduino.write(bytes(x, 'utf-8')) 
+	   time.sleep(0.05) 
+	   data = arduino.readline() 
+	   return data 
+while True: 
+	   num = input("Enter a number: ") # Taking input from user 
+	   value = write_read(num) 
+	   print(value) # printing the value 
